03a72ca5bfce457cca400352786fc448884da970
KatorinDmitry <katorin.dima@yandex.ru>
2026-01-24 20:56:27 +0300

    add more attributes for UserRead

--- services/user_service/core/schemas/user.py
+++ services/user_service/core/schemas/user.py
@@ -1,3 +1,7 @@
+from uuid import UUID
+from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel, EmailStr, ConfigDict
 
 
@@ -13,5 +17,16 @@ class UserRead(UserBase):
     model_config = ConfigDict(
         from_attributes=True
     )
-    id: int
+    id: UUID
+    is_active: bool
+    is_deleted: bool
+    created_at: datetime
+    updated_at: datetime
+
+    # Nullable fields
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    display_name: Optional[str] = None
+    avatar_url: Optional[str] = None
+    deleted_at: Optional[datetime] = None
 
