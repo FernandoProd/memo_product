a53df7be0c3cfa73fa2cdc5e14d75cfca492c4c3
KatorinD_at_work <katorin.dima@yandex.ru>
2026-01-27 12:58:26 +0300

    created business logic of users service

---
+++ services/user_service/core/busines_logic/user.py
@@ -0,0 +1,20 @@
+import crud.users
+from core.models import User
+from core.schemas.user import UserCreateInternal
+from crud.users import UserCreate
+from core.security.auth_utils import hash_password
+from sqlalchemy.ext.asyncio import AsyncSession
+
+class UserService:
+    async def create_user_with_hash(self, session: AsyncSession, user_data: UserCreate) -> User:
+        hashed_pwd = hash_password(user_data.password)
+        user_dict = UserCreateInternal(
+            username = user_data.username,
+            email = user_data.email,
+            hashed_password = hashed_pwd,
+        )
+        user = await crud.users.create_user(
+            session=session,
+            user_create = user_dict,
+        )
+        return user
