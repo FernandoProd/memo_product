376a40139c07d367dc51a217a5452ad04a6542fb
KatorinD_at_work <katorin.dima@yandex.ru>
2026-01-28 16:20:51 +0300

    added pydantic schemas for auth service

---
+++ services/auth_service/core/schemas/schemas.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel, EmailStr
+
+
+class TokenInfo(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str = "Bearer"
+
+
+class TokenResponse(BaseModel):
+    access_token: str
+    refresh_token: str | None = None
+    token_type: str = "Bearer"
+
+
+
+class TokenData(BaseModel):
+    pass
+
+
+class LoginRequest(BaseModel):
+    username: str
+    email: str
+    password: str
+
+
+class LogoutRequest(BaseModel):
+    pass
+
+
+class RefreshTokenRequest(BaseModel):
+    pass
+
+
+class UserSchema(BaseModel):
+    # model_config = ConfigDict(strict=True)
+    user_id: str
+    username: str
+    email: EmailStr | None = None
+    is_active: bool = True
