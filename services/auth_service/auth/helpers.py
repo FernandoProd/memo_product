d9fa5d261f9f82fd48f2954b5b13b33785af2b83
KatorinDmitry <katorin.dima@yandex.ru>
2026-01-28 22:12:23 +0300

    added function for creating jwt tokens and used it for creating access and refresh tokens

--- services/auth_service/auth/helpers.py
+++ services/auth_service/auth/helpers.py
@@ -1,4 +1,8 @@
+from core.config import settings
 from core.schemas.schemas import UserSchema
+from datetime import timedelta
+
+from utils import jwt_utils
 
 TOKEN_TYPE_FIELD = "type"
 ACCESS_TOKEN_TYPE = "access"
@@ -7,21 +11,23 @@ REFRESH_TOKEN_TYPE = "refresh"
 def create_jwt(
         token_type: str,
         token_data: dict,
+        expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+        expire_timedelta: timedelta | None = None,
 ) -> str:
-    # jwt_payload = {
-    #     TOKEN_TYPE_FIELD: token_type
-    # }
-    # jwt_payload.update(token_data)
-    # return auth_utils.encode_jwt(
-    #     payload=jwt_payload,
-    #     expire_minutes=expire_minutes,
-    #     expire_timedelta=expire_timedelta,
-    # )
+    jwt_payload = {
+        TOKEN_TYPE_FIELD: token_type
+    }
+    jwt_payload.update(token_data)
+    return jwt_utils.encode_jwt(
+        payload=jwt_payload,
+        expire_minutes=expire_minutes,
+        expire_timedelta=expire_timedelta,
+    )
     pass
 
 def create_access_token(
         user: UserSchema # По факту вызывается Endpoint verify из user_service
-):
+) -> str:
     jwt_payload = {
         "sub": user.user_id,
         "email": user.email,
@@ -29,3 +35,21 @@ def create_access_token(
         "roles": user.roles,
 
     }
+
+    return create_jwt(
+        token_type=ACCESS_TOKEN_TYPE,
+        token_data=jwt_payload,
+        expire_minutes = settings.auth_jwt.access_token_expire_minutes,
+    )
+
+
+def create_refresh_token(user: UserSchema) -> str:
+    jwt_payload = {
+        "sub": user.user_id,
+    }
+
+    return create_jwt(
+        token_type=REFRESH_TOKEN_TYPE,
+        token_data=jwt_payload,
+        expire_timedelta=timedelta(days=settings.auth_jwt.refresh_token_expire_days),
+    )
