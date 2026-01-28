287d5bf138e0c13b4acfa7bcfccb1b362ec48040
KatorinDmitry <katorin.dima@yandex.ru>
2026-01-28 22:10:31 +0300

    added encode and decode for creating tokens

---
+++ services/auth_service/utils/jwt_utils.py
@@ -0,0 +1,44 @@
+from services.auth_service.core.config import settings
+from datetime import datetime, timedelta
+import jwt
+import uuid
+
+
+def encode_jwt(
+        payload: dict,
+        private_key: str = settings.auth_jwt.private_key_path.read_text(),
+        algorithm: str = settings.auth_jwt.algorithm,
+        expire_timedelta: timedelta | None = None,
+        expire_minutes: int = settings.auth_jwt.access_token_expire_minutes,
+):
+    to_encode = payload.copy()
+    now = datetime.utcnow()
+    if expire_timedelta:
+        expire = now + expire_timedelta
+    else:
+        expire = now + timedelta(minutes=expire_minutes)
+    to_encode.update(
+        exp=expire,
+        iat=now, # issue at
+        jti=str(uuid.uuid4())
+    )
+
+    encoded = jwt.encode(
+        to_encode, # payload но с expire
+        private_key,
+        algorithm=algorithm,
+    )
+    return encoded
+
+
+def decode_jwt(
+        token: str | bytes,
+        public_key: str = settings.auth_jwt.public_key_path.read_text(),
+        algorithm: str = settings.auth_jwt.algorithm,
+):
+    decoded = jwt.decode(
+        token,
+        public_key,
+        algorithms=[algorithm]
+    )
+    return decoded
