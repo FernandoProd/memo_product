082772567709e71b4884f9e2b4ef1ccc6968c35c
KatorinD_at_work <katorin.dima@yandex.ru>
2026-01-29 16:36:11 +0300

    added verify endpoint: take user id and pwd from swagger and you'll be able to get user payload

--- services/user_service/api/api_v1/users.py
+++ services/user_service/api/api_v1/users.py
@@ -6,6 +6,8 @@ from services.user_service.core.busines_logic.user import UserService
 from services.user_service.core.models import db_helper
 from services.user_service.core.schemas.user import UserCreate, UserRead
 from crud import users as users_crud
+from services.user_service.core.security.auth_utils import validate_password
+from fastapi import HTTPException
 
 router = APIRouter(tags=["Users"])
 
@@ -31,24 +33,32 @@ class UserSchemaForAuth(BaseModel):
     sub: str
     email: EmailStr
     username: str
-    roles: dict # Можно хранить dict = {"role_auth_user": True, "role_vip_user": False, "role_admin": False} - возможно стоит делать схему этого словаря
+    roles: dict = {} # Можно хранить dict = {"role_auth_user": True, "role_vip_user": False, "role_admin": False} - возможно стоит делать схему этого словаря
 
 
 
 # Эта шляпа нужна для того, чтобы дать auth_service payload для jwt, если пароль сходится (можно еще где-нибудь применять)
-@router.post("verify/")
-async def verify_user_pwd(user_id: str, password: str) -> UserSchemaForAuth:
+@router.post("/verify")
+async def verify_user_pwd(
+        user_id: str,
+        password: str,
+        session: AsyncSession = Depends(db_helper.session_getter),
+) -> UserSchemaForAuth:
     service = UserService()
-    check_user = await service.get_user_by_id(user_id)
+    check_user = await service.get_user_by_id(session=session, user_id=user_id)
 
-    if password != check_user.password:
-        raise "password is not valid"
 
-    user_for_auth = {
-        "sub": check_user.user_id,
-        "email": check_user.email,
-        "username": check_user.username,
-        "roles": check_user.roles,  # Пока что нигде не добавлены((
-    }
+    if not validate_password(
+            password=password,
+            hashed_password=check_user.hashed_password
+    ):
+        raise HTTPException(status_code=401 , detail="password is not valid")
+
+    user_for_auth = UserSchemaForAuth(
+        sub=str(check_user.id),
+        email=check_user.email,
+        username=check_user.username,
+        # roles=check_user.roles,  # Пока что нигде не добавлены((
+    )
 
     return user_for_auth
