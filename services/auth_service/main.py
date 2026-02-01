e6748222798fa3748a5293cfcb9ae9e7748c5655
KatorinDmitry <katorin.dima@yandex.ru>
2026-02-01 19:00:31 +0300

    added middleware

--- services/auth_service/main.py
+++ services/auth_service/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from services.auth_service.api.api_v1.endpoints.auth import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(title="Auth Service")
+app = FastAPI(
+    title="Auth Service",
+    swagger_ui_parameters={
+        "persistAuthorization": True,
+        "withCredentials": True,
+    }
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8001", "http://0.0.0.0:8001"],  # адрес твоего frontend/Swagger
+    allow_credentials=True,  # ← САМОЕ ВАЖНОЕ!
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 
