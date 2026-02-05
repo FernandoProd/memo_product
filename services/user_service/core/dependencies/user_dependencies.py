df9acb7fe211dc4007c611092981458d3a836101
KatorinDmitry <katorin.dima@yandex.ru>
2026-02-05 22:03:33 +0300

    added dependency for getting current user as get_current_user

---
+++ services/user_service/core/dependencies/user_dependencies.py
@@ -0,0 +1,38 @@
+from fastapi import HTTPException, Header, Depends
+from services.libs.http_client.auth_client import AuthServiceClient
+from typing import Optional
+
+
+def get_auth_client() -> AuthServiceClient:
+        return AuthServiceClient()
+
+
+async def get_current_user(
+            authorization: Optional[str] = Header(None),
+            http_client: AuthServiceClient = Depends(get_auth_client)
+) -> dict:
+    if not authorization.startswith("Bearer "):
+        raise HTTPException(status_code=401, detail="Invalid authorization header")
+
+    if not authorization or not authorization.startswith("Bearer "):
+        raise HTTPException(
+            status_code=401,
+            detail="Missing or invalid Authorization header"
+        )
+
+    token = authorization.split("Bearer ")[1]
+
+    try:
+        response = await http_client.get_current_user(token)
+
+        if response.status_code != 200:
+            raise HTTPException(
+                status_code=response.status_code,
+                detail="Invalid token"
+            )
+
+        auth_data = response.json()
+        return auth_data  # {"user_id": "...", "email": "...", ...}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
