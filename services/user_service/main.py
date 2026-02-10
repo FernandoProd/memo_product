67de89f9c04e66c1f33367e3aa368897bf9485eb
KatorinDmitry <katorin.dima@yandex.ru>
2026-02-10 22:17:51 +0300

    added log conf to main file

--- services/user_service/main.py
+++ services/user_service/main.py
@@ -1,3 +1,22 @@
+import logging
+import sys
+
+
+# logging.basicConfig(
+#     level=logging.DEBUG,
+#     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#     handlers=[
+#         logging.FileHandler('app.log'),
+#         logging.StreamHandler(sys.stdout)
+#     ]
+# )
+#
+# # Устанавливаем уровень для библиотек
+# logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+# logging.getLogger('uvicorn').setLevel(logging.INFO)
+
+
+
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -7,7 +26,10 @@ from fastapi.responses import ORJSONResponse
 from api import router as api_router
 from core.config import settings
 from core.models.db_helper import db_helper
+from services.user_service.core.logging.log_config import setup_logging
 
+setup_logging()
+logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -19,17 +41,14 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()
 
 
-# from services.user_service.core.logging.log_config import setup_logging
-#
-# setup_logging()
-
+logger.debug("Отладочное сообщение")
 
 # ORJSONResponse быстрее отправляет байтики
 main_app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
 )
-
+logger.debug("Отладочное сообщение")
 main_app.include_router(api_router, prefix=settings.api.prefix)
 
 
@@ -38,7 +57,7 @@ if __name__ == "__main__":
         "main:main_app",
         host=settings.run.host,
         port=settings.run.port,
-        log_level="info",
+        log_level=None,
         reload=True,
     )
 
