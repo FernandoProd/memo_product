e6224497d706420b50b88e563d5ee57c0caaeb2d
KatorinD_at_work <katorin.dima@yandex.ru>
2026-02-17 16:41:59 +0300

    add lifespan for redis

--- services/auth_service/app/main.py
+++ services/auth_service/app/main.py
@@ -2,13 +2,25 @@ from fastapi import FastAPI
 from services.auth_service.app.api.api_v1.endpoints.auth import router as auth_router
 from fastapi.middleware.cors import CORSMiddleware
 from memo_libs.logging.log_config import setup_logging
+from services.auth_service.app.models.redis import get_redis_pool
+from contextlib import asynccontextmanager
 import logging
 
 
 setup_logging()
 logger = logging.getLogger(__name__)
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Создаём пул соединений
+    redis_client = await get_redis_pool()
+    app.state.redis_client = redis_client
+    yield
+    # Закрываем соединения
+    await redis_client.close()
 app = FastAPI(
     title="Auth Service",
+    lifespan=lifespan,
     swagger_ui_parameters={
         "persistAuthorization": True,
         "withCredentials": True,
