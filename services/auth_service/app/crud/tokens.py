e8246e9cba926a6f903718e682e34420b04ba01d
KatorinDmitry <katorin.dima@yandex.ru>
2026-02-19 21:58:11 +0300

    add function for getting hash of refresh token from db by cookie's rtoken

--- services/auth_service/app/crud/tokens.py
+++ services/auth_service/app/crud/tokens.py
@@ -1,6 +1,7 @@
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from services.auth_service.app.models import RefreshToken
-
+from sqlalchemy import select
 
 # def get_token_lifetime_info(token: str) -> dict:
 #     """
@@ -53,3 +54,22 @@ async def add_token(
     session.add(token_data)
     await session.commit()
     return token_data
+
+
+async def get_refresh_token(
+        session: AsyncSession,
+        hashed_refresh_token: str
+) -> RefreshToken:
+    try:
+        stmt = select(RefreshToken).where(
+            RefreshToken.token_hash == hashed_refresh_token,
+            RefreshToken.revoked == False
+        )
+        result = await session.execute(stmt)
+        return result.scalar_one_or_none()
+    except Exception as e:
+        raise e
+
+
+async def get_user_id_by_token_hash():
+    pass
