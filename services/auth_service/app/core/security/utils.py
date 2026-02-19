01203d8ca377b32ccaf148a1cc25df5c0b05fdbb
KatorinDmitry <katorin.dima@yandex.ru>
2026-02-19 21:56:26 +0300

    changing hash and validate functions because its makes difficult to compare 2 hashed tokens

--- services/auth_service/app/core/security/utils.py
+++ services/auth_service/app/core/security/utils.py
@@ -3,7 +3,7 @@ import bcrypt
 
 
 
-def hash_token(
+def hash_token_for_better_security(
         token: str,
 ) -> str:
     # First we'll hash all of jwt_refresh_token with SHA-256, because bcrypt can hash only 72 bytes
@@ -15,9 +15,17 @@ def hash_token(
 
     return hashed.decode('utf-8')
 
-def validate_token(
+def validate_token_for_better_security(
         token: str,
         hashed_token: str
 ) -> bool:
     sha256_hash = hashlib.sha256(token.encode('utf-8')).digest()
     return bcrypt.checkpw(sha256_hash, hashed_token.encode('utf-8'))
+
+
+# Я ввожу это, так как соль каждый раз создает новый токен и сравнивать рефреш + hashed_refresh невозможно
+def hash_token(token: str) -> str:
+    return hashlib.sha256(token.encode('utf-8')).hexdigest()
+
+def validate_token(token: str, hashed_token: str) -> bool:
+    return hash_token(token) == hashed_token
