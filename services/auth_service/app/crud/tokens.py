0ff3ebf14978a2293ab9d12deb880e4bde1d4f48
KatorinDmitry <katorin.dima@yandex.ru>
2026-02-20 20:26:38 +0300

    add functional of revoking token with connect to db and upgrade tokens status

--- services/auth_service/app/crud/tokens.py
+++ services/auth_service/app/crud/tokens.py
@@ -1,50 +1,6 @@
-from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
 from services.auth_service.app.models import RefreshToken
-from sqlalchemy import select
-
-# def get_token_lifetime_info(token: str) -> dict:
-#     """
-#     Возвращает дату выдачи и окончания срока действия JWT-токена.
-#     """
-#     payload = decode_jwt(token=token)
-#
-#     iat_timestamp = payload.get("iat")
-#     exp_timestamp = payload.get("exp")
-#
-#     issued_at = datetime.utcfromtimestamp(iat_timestamp).strftime('%Y-%m-%d %H:%M:%S UTC') if iat_timestamp else None
-#     expires_at = datetime.utcfromtimestamp(exp_timestamp).strftime('%Y-%m-%d %H:%M:%S UTC') if exp_timestamp else None
-#
-#     return {
-#         "issued_at": issued_at,
-#         "expires_at": expires_at,
-#         "is_expired": exp_timestamp < datetime.utcnow().timestamp() if exp_timestamp else None
-#     }
-
-
-
-# async def add_token_info_into_db(
-#         session: AsyncSession,
-#         refresh_token: str,
-# ) -> RefreshToken:
-#     payload = decode_jwt(token=refresh_token)
-#     user_id = payload.get("sub")
-#     # iat_timestamp = payload.get("iat")
-#     exp_timestamp = payload.get("exp")
-#     # issued_at = datetime.utcfromtimestamp(iat_timestamp).strftime('%Y-%m-%d %H:%M:%S UTC') if iat_timestamp else None
-#     expires_at = datetime.fromtimestamp(exp_timestamp, tz=timezone.utc)
-#
-#
-#
-#     hashed_token = hash_token(refresh_token)
-#     token_model = RefreshToken(
-#         user_id=user_id,
-#         token_hash=str(hashed_token),
-#         expires_at=expires_at,
-#     )
-#     session.add(token_model)
-#     await session.commit()
-#     return token_model
+from sqlalchemy import select, update
 
 
 async def add_token(
@@ -52,7 +8,7 @@ async def add_token(
         token_data: RefreshToken
 ):
     session.add(token_data)
-    await session.commit()
+    await session.commit() # Можно делать его делать на уровень выше, чтобы помещать в ттранзакцию больше функционала
     return token_data
 
 
@@ -73,3 +29,26 @@ async def get_refresh_token(
 
 async def get_user_id_by_token_hash():
     pass
+
+
+async def revoke_token(
+        session: AsyncSession,
+        hashed_refresh_token: str
+) -> bool:
+    stmt = (
+        update(RefreshToken)
+        .where(
+            RefreshToken.token_hash == hashed_refresh_token,
+            RefreshToken.revoked == False
+        )
+        .values(revoked=True)
+        .execution_options(synchronize_session="fetch")  # для синхронизации сессии
+    )
+
+    result = await session.execute(stmt)
+    if result.rowcount == 0:
+        raise ValueError("Refresh token not found or already revoked")
+
+    await session.commit() # Можно делать на уровень выше
+
+    return True
