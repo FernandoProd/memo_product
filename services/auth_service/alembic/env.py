11e969b2fad2466a53371e793ffdfd15d3c43942
KatorinDmitry <katorin.dima@yandex.ru>
2026-02-22 13:45:49 +0300

    changes: some changes in imports because it wasn't working

--- services/auth_service/alembic/env.py
+++ services/auth_service/alembic/env.py
@@ -6,8 +6,8 @@ from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
-from app.models import Base
-from app.core.config import settings
+from services.auth_service.app.models import Base
+from services.auth_service.app.core.config import settings
 
 
 # this is the Alembic Config object, which provides
