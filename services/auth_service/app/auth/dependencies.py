a2874d5852bc7e3a9ae72ff508b3bc18a41f5e7e
KatorinD_at_work <katorin.dima@yandex.ru>
2026-02-25 16:07:45 +0300

    refactor(dependencies): delete unused things and change exceptions

--- services/auth_service/app/auth/dependencies.py
+++ services/auth_service/app/auth/dependencies.py
@@ -1,7 +1,7 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError
-from jwt import InvalidTokenError
+
 from services.auth_service.app.utils import jwt_utils
 from services.auth_service.app.schemas.schemas import UserSchema
 
@@ -9,14 +9,11 @@ from services.auth_service.app.schemas.schemas import UserSchema
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login/")
 
 def get_current_token_payload(
-        # credentials: HTTPAuthorizationCredentials = Depends(http_bearer), #Вместо токена приходят credentials
         token: str = Depends(oauth2_scheme)
 ) -> UserSchema:
-    # token = credentials.credentials
-    # print(token)
     try:
         payload = jwt_utils.decode_jwt(token)
-    except InvalidTokenError as e:
+    except JWTError as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"invalid token error: {e}"
@@ -35,16 +32,5 @@ def verify_users_token(token: str):
             detail="Invalid token"
         )
 
-
-# Зависимость для получения актуальной информации о пользователе
-class UserGetterWithToken:
-    def __init__(self, token_type: str):
-        self.token_type = token_type
-
-    def __call__(
-            self,
-            payload: dict = Depends(get_current_token_payload)
-    ):
-        pass
 
 
