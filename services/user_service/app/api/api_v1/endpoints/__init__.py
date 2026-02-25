eb6e8defd41e5ffddba12b6ca3c286535b4a36d6
KatorinD_at_work <katorin.dima@yandex.ru>
2026-02-25 13:42:37 +0300

    delete: clean up bed comments

--- services/user_service/app/api/api_v1/endpoints/__init__.py
+++ services/user_service/app/api/api_v1/endpoints/__init__.py
@@ -3,7 +3,6 @@ from fastapi import APIRouter
 from services.user_service.app.api.api_v1.endpoints.users import router as users_router
 from services.user_service.app.core.config import settings
 
-
 router = APIRouter(
     prefix=settings.api.v1.prefix,
 )
@@ -11,7 +10,3 @@ router.include_router(
     users_router,
     prefix=settings.api.v1.users
 )
-
-
-# print(router.prefix)
-# services.user_service.app.api.api_v1.endpoints
